416f6180b1de2c870eea412d0c8691274d88ba3a
Jose Daniel Quiroz <jqhenaod14@gmail.com>
2020-05-14 13:30:49 -0500

    updated store controller

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -3,26 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Store;
+use Carbon\Carbon;
 
 class StoreController extends Controller
 {
     public function Stores(){
-        return Store::all();
+        return DB::table('stores')->get();
     }
     public function Show(Store $store) {
         return $store;
     }
     public function CreateStore(Request $request) {
-        $store = Store::create($request->all());
-        return response()->json($store, 201);
+        $name = $request->name;
+        $oppening_date = $request->oppening_date;
+        $store = DB::insert('INSERT INTO stores (name, oppening_date) VALUES (?, ?)', array($name, $oppening_date));
+        $content;
+        return response()->json('Created', 201);
     }
     public function UpdateStore(Request $request, Store $store) {
-        $store->update($request->all());
-        return response()->json($store, 200);
+        $store = Store::findOrFail($store->id);
+        $store->name = $request['name'];
+        $store->oppening_date = $store->oppening_date;
+        $store->save();
+        return response()->json('Updated', 200);
     }
     public function DeleteStore(Store $store) {
         $store->delete();
-        return response()->json('Deleted', 204);
+        return response()->json('Deleted', 200);
     }
 }
